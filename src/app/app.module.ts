c57c94d4d6f4da60d594a39490cde2c4d65b8fc4
Lotus Biswas <102558895+dev-lotus@users.noreply.github.com>
2022-06-05 22:48:51 +0530

    Recent Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent,
-    NavbarComponent
+    MainComponent
   ],
   imports: [
     BrowserModule,
